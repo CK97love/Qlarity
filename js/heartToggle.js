6d5d2d0be970723ac5fd6f6a79574cee10685626
Jason Ho <jason.c.ho1999@gmail.com>
2018-11-13 13:57:24 -0800

    Fixed bug for more than 1 digit likes

--- js/heartToggle.js
+++ js/heartToggle.js
@@ -2,7 +2,17 @@ function changeImage(e, i) {
     x = document.getElementsByClassName("heartCount");
     console.log(x);
     console.log(x[i].innerHTML[[0]]);
-    var numLikes = parseInt(x[i].innerHTML[0], 10);
+    var c = 0;
+    var number = "";
+
+    while(x[i].innerHTML[c] != 'L') {
+        if(x[i].innerHTML[c] != ' ') {
+            number += x[i].innerHTML[c];
+        }
+        c++;
+    }
+    var numLikes = parseInt(number);
+
     if(e.getAttribute('src') == 'images/heart.png') {
         e.setAttribute('src', 'images/heartfilled.png');
         e.setAttribute('height', '15px');
