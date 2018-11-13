6b0fc8d38eb5555d006ee33d67b26895d32e7f2b
Jason Ho <jason.c.ho1999@gmail.com>
2018-11-13 12:06:02 -0800

    Now only have to change text and number parameter

--- js/heartToggle.js
+++ js/heartToggle.js
@@ -1,18 +1,20 @@
-function changeImage(e, num, i) {
+function changeImage(e, i) {
     x = document.getElementsByClassName("heartCount");
     console.log(x);
+    console.log(x[i].innerHTML[[0]]);
+    var numLikes = parseInt(x[i].innerHTML[0], 10);
     if(e.getAttribute('src') == 'images/heart.png') {
         e.setAttribute('src', 'images/heartfilled.png');
         e.setAttribute('height', '15px');
         e.setAttribute('width', '15px');
-        num = num + 1;
-        x[i].innerHTML = num + " Likes";
+        numLikes = numLikes + 1;
+        x[i].innerHTML = numLikes + " Likes";
     } else {
         e.setAttribute('src', 'images/heart.png');
         e.setAttribute('height', '20px');
         e.setAttribute('width', '20px');
-        num = num;
-        x[i].innerHTML = num + " Likes";
+        numLikes = numLikes - 1;
+        x[i].innerHTML = numLikes + " Likes";
     }
 }
 
