bb8e12c983c5b7afeadbe9a05c8ba635323a256c
Jason Ho <jason.c.ho1999@gmail.com>
2018-11-15 21:59:45 -0800

    Comment on first two posts

--- js/comment.js
+++ js/comment.js
@@ -3,5 +3,6 @@ function postComment(i) {
     var texts = document.getElementsByClassName("replyBox");
     var commentText = texts[i];
     localStorage.setItem("commentText", commentText.value);
+    localStorage.setItem("index", i);
     location.reload(true);
 }
--- js/postComment.js
+++ js/postComment.js
@@ -3,7 +3,6 @@ function writeComment() {
     var currTime = new Date().toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
 
     if(commentText) {
-        //var div = document.getElementsByClassName("collapse");
         var div = document.getElementsByClassName("next-comment")[0];
         console.log(div);
         var divClone = div.cloneNode(true);
@@ -18,9 +17,37 @@ function writeComment() {
         var changeTime = divClone.querySelector('#time');
         changeTime.innerHTML = currTime;
 
-        var collapse = document.getElementsByClassName("collapse")[0];
+        var index = localStorage.getItem("index");
+        var buttons = document.getElementsByClassName("btn");
+        var comment = buttons[index];
+        console.log(comment);
+        var commentString = (String)(comment.innerHTML);
+        console.log(commentString);
+        var numOfComments = 0;
+        for(i = 0; i < commentString.length; i++) {
+            if(parseInt(commentString.charAt(i))) {
+                numOfComments = parseInt(commentString.charAt(i));
+            }
+        }
+
+        console.log(numOfComments);
+        
+        var collapse = document.getElementsByClassName("collapse")[index];
         var locationOfPost = collapse.querySelector("#postBody");
-        locationOfPost.insertAdjacentElement('afterend', divClone);
+        if(numOfComments == 1) {
+            console.log(index);
+            locationOfPost.insertAdjacentElement('afterend', divClone);
+        }
+        if(numOfComments == 2) {
+            locationOfPost = collapse.getElementsByClassName("next-comment")[0];
+            console.log(locationOfPost);
+            locationOfPost.insertAdjacentElement('afterend', divClone);
+        }
+        if(numOfComments == 0) {
+            locationOfPost = collapse.getElementsByClassName("reply")[0];
+            console.log(locationOfPost);
+            locationOfPost.insertAdjacentElement('beforebegin', divClone);
+        }
         console.log(collapse.innerHTML); 
 
     }
